9b5f2fe8af7dd0c8e8418398181e4746fcd4c72a
Henri Verroken <henriverroken@gmail.com>
2016-10-03 09:33:12 +0100

    Add utility function to convert bytes to a hex string

--- src/System/Hardware/ELM327/Utils/Hex.hs
+++ src/System/Hardware/ELM327/Utils/Hex.hs
@@ -1,9 +1,11 @@
 -- | Collection of functions to deal with hexadecimal encoding.
 module System.Hardware.ELM327.Utils.Hex where
 
+import Data.List (intercalate)
 import Data.List.Split (chunksOf)
 import Data.Word (Word8)
 import Numeric (readHex)
+import Text.Printf (printf)
 
 -- | Convert an even list of hexadecimal characters to a list of Word8 values.
 --
@@ -15,3 +17,9 @@ hexToBytes = mapM pairToWord8 . chunksOf 2
     pairToWord8 s@[_, _] | [(x, "")] <- readHex s = Just x
                          | otherwise = Nothing
     pairToWord8 _ = Nothing
+
+-- | Convert a list of bytes to a hexadecimal string.
+--
+-- Use seperator as a seperator between each byte in the resulting string.
+bytesToHex :: String -> [Word8] -> String
+bytesToHex sep = intercalate sep . map (printf "%02X")
