d81a65e766e9afedd32243c28579d7cb80eb3098
Henri Verroken <henriverroken@gmail.com>
2016-10-03 13:24:00 +0100

    Handle AT commands in simulator

--- src/System/Hardware/ELM327/Simulator.hs
+++ src/System/Hardware/ELM327/Simulator.hs
@@ -1,12 +1,33 @@
 -- | Module that simulates an ELM327 device.
-module System.Hardware.ELM327.Simulator where
+module System.Hardware.ELM327.Simulator (
+  -- * Initializing the simulator
+  Simulator(..)
+, defaultSimulator
 
-import Control.Lens (Lens', lens, (^.), (^?), (.~))
+  -- * Interacting with the simulator
+, handle
+
+  -- * Attributes of the simulator
+, ConnectedProtocol(..)
+, Echo(..)
+, obdBus
+, chosenProtocol
+, connectedProtocol
+, echo
+, pin2Voltage
+, versionID
+) where
+
+import Control.Lens (Lens', lens, (^.), (^?), (.~), re)
 import Control.Monad.State (State, runState, get, put)
 import Data.Char (toUpper)
+import Text.Printf (printf)
 
+import Numeric.Units.Dimensional.Prelude (ElectricPotential, volt, (*~), (/~))
 
-import System.Hardware.ELM327.Commands (Command(..), Protocol(..), command, obdMode, obdPID)
+import System.Hardware.ELM327.Commands (AT(..), Command(..), command,
+                                        Protocol(..), protocol,
+                                        obdMode, obdPID)
 import System.Hardware.ELM327.Simulator.OBDBus (OBDBus)
 import System.Hardware.ELM327.Utils.Hex (bytesToHex)
 import qualified System.Hardware.ELM327.Simulator.OBDBus as OBDBus
@@ -16,6 +37,7 @@ data Simulator bus = Simulator { _obdBus :: bus
                                , _chosenProtocol :: Protocol
                                , _connectedProtocol :: ConnectedProtocol
                                , _echo :: Echo
+                               , _pin2Voltage :: ElectricPotential Double
                                , _versionID :: String }
 
 -- | The default simulator, initialized with a given protocol
@@ -24,6 +46,7 @@ defaultSimulator b = Simulator { _obdBus = b
                                , _chosenProtocol = AutomaticProtocol
                                , _connectedProtocol = NotConnected
                                , _echo = EchoOff
+                               , _pin2Voltage = 12.3 *~ volt
                                , _versionID = "ELM327 v2.1" }
 
 -- | Make the simulator handle a command.
@@ -40,7 +63,12 @@ handle cmd = maybe (return "?") handle' $ sanitize cmd
         case resp of
             Nothing -> return $ reply "NO DATA"
             Just v -> return . reply . bytesToHex " " $ [obdMode x + 0x40, obdPID x] ++ v
-    handle' _ = return "OK"
+    handle' (AT ATDescribeProtocolNumber) = reply <$> describeProtocolNumber
+    handle' (AT ATEchoOff) = reply <$> echoOff
+    handle' (AT ATReadVoltage) = reply <$> readVoltage
+    handle' (AT ATResetAll) = reply <$> resetAll
+    handle' (AT (ATSelectProtocol p)) = reply <$> selectProtocol p
+    handle' (AT ATVersionID) = reply . (^. versionID) <$> get
 
     reply = (++ "\r>")
 
@@ -69,6 +97,38 @@ connectedProtocol = lens _connectedProtocol $ \s x -> s { _connectedProtocol = x
 echo :: Lens' (Simulator bus) Echo
 echo = lens _echo $ \s x -> s { _echo = x }
 
+-- | The voltage on pin 2 (most likely the battery voltage)
+pin2Voltage :: Lens' (Simulator bus) (ElectricPotential Double)
+pin2Voltage = lens _pin2Voltage $ \s x -> s { _pin2Voltage = x }
+
 -- | The version ID of the ELM327.
 versionID :: Lens' (Simulator bus) String
 versionID = lens _versionID $ \s x -> s { _versionID = x }
+
+-- | If the command is just a setting change, the ELM327 will reply with OK.
+replyOK :: State (Simulator bus) String
+replyOK = return "OK"
+
+-- | Describe the protocol number
+describeProtocolNumber :: State (Simulator bus) String
+describeProtocolNumber = do
+    cp <- (^. connectedProtocol) <$> get
+    case cp of NotConnected -> return $ "A" ++ AutomaticProtocol ^. re protocol
+               ManuallyChosen p -> return $ p ^. re protocol
+               AutomaticallyChosen p -> return $ "A" ++ p ^. re protocol
+
+-- | Turn echo off
+echoOff :: State (Simulator bus) String
+echoOff = (echo .~ EchoOff) <$> get >>= put >> replyOK
+
+-- | Read the battery voltage
+readVoltage :: State (Simulator bus) String
+readVoltage = printf "%.1f" . (/~ volt) . (^. pin2Voltage) <$> get
+
+-- | Fully reset the device
+resetAll :: OBDBus bus => State (Simulator bus) String
+resetAll = defaultSimulator . (^. obdBus) <$> get >>= put >> return ""
+
+-- | Select a protocol
+selectProtocol :: Protocol -> State (Simulator bus) String
+selectProtocol p = (chosenProtocol .~ p) <$> get >>= put >> replyOK
