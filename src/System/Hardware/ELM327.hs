4afe48aeaf0d384fa8a430b09a55a3f145fcb2a5
Henri Verroken <henriverroken@gmail.com>
2016-10-05 07:55:33 +0100

    Create serial connection to ELM327 device

--- src/System/Hardware/ELM327.hs
+++ src/System/Hardware/ELM327.hs
@@ -1,111 +1,40 @@
 -- | Basic interface for communication with the ELM327.
 module System.Hardware.ELM327 where
 
-import Control.Monad (when)
-import Control.Lens (re, (^.), (^?))
-import Data.ByteString.Char8 (ByteString)
-import Data.Char (isHexDigit)
-import Data.List (stripPrefix)
-import Data.Maybe (fromMaybe)
-import Data.Word (Word8)
 import qualified Data.ByteString.Char8 as Char8
+
+import System.IO.Streams (makeInputStream, makeOutputStream)
 
 import System.Hardware.Serialport (CommSpeed(..),
                                    FlowControl(..),
                                    Parity(..),
-                                   SerialPort,
                                    SerialPortSettings(..),
                                    StopBits(..),
                                    closeSerial,
                                    openSerial)
 import qualified System.Hardware.Serialport as Port
 
-import System.Hardware.ELM327.Commands (AT,
-                                        Command(..),
-                                        OBD,
-                                        command,
-                                        obdMode,
-                                        obdPID)
-import System.Hardware.ELM327.Errors (OBDError(..),
-                                      OBDDecodeError(..),
-                                      obdErrorMessage)
-import System.Hardware.ELM327.Utils.Hex (hexToBytes)
-import System.Hardware.ELM327.Utils.Monad (maybeToLeft, maybeToRight)
-
--- | An established connection to an ELM327 device.
-newtype Con = Con SerialPort
+import System.Hardware.ELM327.Connection (Con(..))
 
 -- | Connect to an ELM327 device.
-connect :: FilePath -> IO Con
-connect fp =
+connect :: FilePath -> IO (Con, IO ())
+connect fp = do
     let s = SerialPortSettings { commSpeed = CS38400
                                , bitsPerWord = 8
                                , stopb = One
                                , parity = NoParity
                                , flowControl = NoFlowControl
-                               , timeout = 2 } in
-    Con <$> openSerial fp s
-
--- | Close an ELM327 connection.
-close :: Con -> IO ()
-close (Con port) = closeSerial port
-
--- | Send an ELM327 command.
-send :: Con -> Command -> IO ()
-send con cmd = do
-    sendString con $ cmd ^. re command
-    sendString con "\r"
-    flush con
-
--- | Send all bytes to the ELM327.
-sendBytes :: Con -> ByteString -> IO ()
-sendBytes con@(Con port) x = do
-    n <- Port.send port x
-    when (n /= Char8.length x) $ sendBytes con (Char8.drop n x)
-
--- | Send a string to the ELM327.
-sendString :: Con -> String -> IO ()
-sendString con = sendBytes con . Char8.pack
-
--- | Receive an ELM327 response as a byte string.
-recv :: Con -> IO (Maybe ByteString)
-recv (Con port) = recv' []
+                               , timeout = 2 }
+    port <- openSerial fp s
+    is <- makeInputStream (produce port)
+    os <- makeOutputStream (consume port)
+    return (Con is os, closeSerial port)
   where
-    recv' xs = do
-        bs <- Port.recv port 1
-        case Char8.unpack bs of
-            [] -> recv' xs
-            ['\r'] -> recv' xs
-            ['\0'] -> recv' xs
-            ['>'] -> return $ ret xs
-            x:_ -> recv' (x:xs)
-    ret [] = Nothing
-    ret xs = Just . Char8.pack . reverse $ xs
+    produce port = Just <$> Port.recv port 8
+    consume _    Nothing = undefined
+    consume port (Just x) = sendAll port x
 
--- | Flush the serial connection
-flush :: Con -> IO ()
-flush (Con port) = Port.flush port
-
--- | Send an 'AT' command and expect a response.
-at :: Con -> AT -> IO (Maybe ByteString)
-at con cmd = send con (AT cmd) >> recv con
-
--- | Send an 'OBD' comand and expect a response.
-obd :: Con -> OBD -> IO (Either OBDError [Word8])
-obd con cmd = do
-    send con (OBD cmd)
-    mbs <- recv con
-    case mbs of
-        Nothing -> return $ Left (OBDDecodeError EmptyResponseError)
-        Just bs -> return . decode $ Char8.unpack bs
-  where
-    decode bs = Right (removeStatusPrefixes bs)
-                >>= \x -> maybeToLeft x (OBDErrorMessage <$> x ^? obdErrorMessage)
-                >>= Right . filter isHexDigit
-                >>= maybeToRight (OBDDecodeError NotEnoughBytesError) . hexToBytes
-                >>= maybeToRight (OBDDecodeError NoResponseHeaderError) . stripHeader
-
-    statusPrefixes = ["SEARCHING..."]
-    removeStatusPrefixes x = foldl stripPrefix' x statusPrefixes
-    stripPrefix' x pref = fromMaybe x (stripPrefix pref x)
-    stripHeader = stripPrefix [0x40 + obdMode cmd, obdPID cmd]
+    sendAll port bs
+        | Char8.null bs = return ()
+        | otherwise = do sent <- Port.send port bs
+                         sendAll port $ Char8.drop sent bs
