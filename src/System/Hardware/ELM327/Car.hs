6a3d9e84c3d60d37e211c8b2735eafb325266aaa
Henri Verroken <henriverroken@gmail.com>
2016-10-07 13:53:49 +0100

    Add basic car type with basic properties

---
+++ src/System/Hardware/ELM327/Car.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+-- | A generic car data type
+module System.Hardware.ELM327.Car where
+
+import Numeric.Units.Dimensional.Prelude
+
+data Car m = Car { engineCoolantTemperature :: m (CelsiusTemperature Double)
+                 , engineRPM :: m (Frequency Double)
+                 , intakeAirTemperature :: m (CelsiusTemperature Double)
+                 , massAirFlowRate :: m (MassFlow Double)
+                 , throttlePosition :: m Double
+                 , vehicleSpeed :: m (Velocity Double) }
