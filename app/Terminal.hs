8d6b7515ed14bc9329d28ebeed495d99b7cf3432
Henri Verroken <henriverroken@gmail.com>
2016-10-07 12:22:30 +0100

    Connect terminal to simulator or real port

--- app/Terminal.hs
+++ app/Terminal.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Main where
 
+import Control.Exception (bracket)
 import Control.Monad (join)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Reader (MonadReader, ReaderT, runReaderT, ask)
@@ -8,13 +9,23 @@ import Control.Monad.Trans.Class (lift)
 import Data.List (isSuffixOf)
 import qualified Data.ByteString.Char8 as Char8
 
+import Options.Applicative (Parser, execParser,
+                            flag, strOption,
+                            help, long, value,
+                            info, progDesc,
+                            (<>))
+import Options.Applicative.Extra (helper)
+
 import System.Console.Haskeline (InputT, MonadException, RunIO(..),
                                  controlIO, runInputT,
                                  defaultSettings, getInputLine)
 
 import System.Hardware.ELM327.Connection (Con, recvRaw, sendString)
-import System.Hardware.ELM327.Simulator (connect, defaultSimulator)
+import System.Hardware.ELM327.Simulator (defaultSimulator)
 import System.Hardware.ELM327.Simulator.OBDBus.VWPolo2007 (stoppedCarBus)
+import qualified System.Hardware.ELM327.Connection as Connection
+import qualified System.Hardware.ELM327 as ELM327
+import qualified System.Hardware.ELM327.Simulator as Simulator
 
 
 newtype Term a = Term { runTerm :: ReaderT Con IO a }
@@ -23,8 +34,30 @@ newtype Term a = Term { runTerm :: ReaderT Con IO a }
 instance MonadException Term where
     controlIO f = join . liftIO $ f (RunIO return)
 
+data ConnectionType = ConnectionTypeActualDevice FilePath
+                    | ConnectionTypeSimulator
+
+connectionType :: Parser ConnectionType
+connectionType = flag ConnectionTypeActualDevice (const ConnectionTypeSimulator) m <*> serialPort
+    where m = long "simulator" <> help "Connect to the simulator instead of a serial port"
+
+serialPort :: Parser FilePath
+serialPort = strOption m
+    where m = long "port" <> value "/dev/ttyUSB0" <> help "The serial port to connect to"
+
 main :: IO ()
-main = connect (defaultSimulator stoppedCarBus) >>= runTerminalWithConnection
+main = execParser i >>= runTerminal
+    where i = info p $ progDesc "Connect a terminal to an ELM327 device or simulator."
+          p = helper <*> connectionType
+
+connect :: ConnectionType -> IO Con
+connect ConnectionTypeSimulator          = Simulator.connect $ defaultSimulator stoppedCarBus
+connect (ConnectionTypeActualDevice dev) = ELM327.connect dev
+
+runTerminal :: ConnectionType -> IO ()
+runTerminal ct = bracket (connect ct)
+                         (\c -> putStrLn "Closing connection..." >> Connection.close c)
+                         (liftIO . runTerminalWithConnection)
 
 runTerminalWithConnection :: Con -> IO ()
 runTerminalWithConnection = runReaderT (runTerm readEvalPrint)
