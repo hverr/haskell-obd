42e9d7596a65cc57140ff3060717b0317f3485b7
Henri Verroken <henriverroken@gmail.com>
2016-10-19 15:16:48 +0000

    Add the README example as code

--- app/Example.hs
+++ app/Example.hs
@@ -1,25 +1,3 @@
-haskell-obd
-===========
-
-[![Build Status](https://travis-ci.org/hverr/haskell-obd.svg?branch=master)](https://travis-ci.org/hverr/haskell-obd)
-[![Hackage](https://img.shields.io/hackage/v/obd.svg?maxAge=2592000)](https://hackage.haskell.org/package/obd)
-[![Stackage Nightly](http://stackage.org/package/obd/badge/nightly)](http://stackage.org/nightly/package/obd)
-[![Stackage LTS](http://stackage.org/package/obd/badge/lts)](http://stackage.org/lts/package/obd)
-
-Haskell library to communicate with OBD-II over ELM327, with
-
-  - type-safe quantities with dimensions
-  - extensible simulator
-  - example terminal as a demo
-
-Usage
------
-
-Look in [`app/Terminal.hs`](app/Terminal.hs) for a very basic example for low-level communication.
-
-The interface to the engine is shown below and in [`app/Example.hs`](app/Example.hs).
-
-```haskell
 module Main where
 
 import Control.Exception (bracket)
@@ -57,4 +35,4 @@ query con = withCon con $ runCarT $ do
     printI key value = do
         v <- show <$> value
         liftIO $ putStrLn $ key ++ ": " ++ v
-```
+
